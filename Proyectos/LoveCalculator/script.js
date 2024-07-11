480670ed4ac45765f76e06a23ba3d7ded86466d7
Paulo Galarza <paulo-123@hotmail.es>
2024-07-10 21:28:21 -0500

    Validacion de datos vacios proyecto 2

---
+++ Proyectos/LoveCalculator/script.js
@@ -0,0 +1,8 @@
+function calculateLove() {
+    /* console.log('Hola mundo'); */
+    const name1 = document.getElementById('name1').value.trim()
+    const name2 = document.getElementById('name2').value.trim()
+   if (name1 ==="" || name2==="") {
+    alert("Ingresa los nombres")
+   }
+}
