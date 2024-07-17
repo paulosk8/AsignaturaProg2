8b9024f8fde92f286524e7ee9ec3dd6c19d4c7ba
Paulo Galarza <paulo-123@hotmail.es>
2024-07-17 15:01:43 -0500

    Agregado logica del porcentaje de compatibilidad

--- Proyectos/LoveCalculator/script.js
+++ Proyectos/LoveCalculator/script.js
@@ -2,7 +2,19 @@ function calculateLove() {
     /* console.log('Hola mundo'); */
     const name1 = document.getElementById('name1').value.trim()
     const name2 = document.getElementById('name2').value.trim()
-   if (name1 ==="" || name2==="") {
+   if (name1 === "" || name2 === "") {
     alert("Ingresa los nombres")
+   }else {
+    const lovePercentage = Math.floor(Math.random() * 101)
+    const result = document.getElementById("result")
+    /* result.innerHTML = "Felicidades " + lovePercentage + "%" */
+    result.innerHTML = `${name1} y ${name2} su porcentaje de amor es: ${lovePercentage}%`
+    if (lovePercentage <30) {
+        result.innerHTML = result.innerHTML + "<br> No eres compatible, vuelve a intentar" 
+    }else if (lovePercentage>=30 && lovePercentage< 70){
+    result.innerHTML = result.innerHTML + "<br> Eres poco compatible"  
+    }else{
+    result.innerHTML = result.innerHTML + "<br> Felicidades eres compatible"
+    }
    }
 }
